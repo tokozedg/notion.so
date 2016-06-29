b52db8949e79f44e5cfe7afb29be479baebd04be
Nick Gartmann <nick.gartmann@gmail.com>
2016-06-29 09:19:05 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const electron = require('electron');
+
+const {app, BrowserWindow} = electron;
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 600});
+  win.loadURL("http://www.notion.so");
+  win.on('closed', () => {
+    win = null;
+  })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('activate', () => {
+  if(win === null) {
+    createWindow();
+  }
+});
